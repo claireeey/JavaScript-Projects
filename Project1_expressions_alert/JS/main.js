4728f5f1114803e27576299bc5e3871c4734a1e4
claireeey <tjxyn1013@gmail.com>
2025-03-10 13:29:16 +0000

    JavaScript-Projects

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("i am cute")
+
+document.write('Lisa told Bart, \'Knock it off, Bart! Or I will tell dad.!\'<br>\'Eat my shorts!\' Bart responded.<br>');
+
+document.write("\"Be who you are and say what you fell,"
+    + " because those who mind don\'t matter and those who matter dont\'t mind.\" "
+    + "-Dr. Seuss<br>");
+
+
+var firstName = "Max";
+var lastName = "Black";
+var message = "Hello, " + firstName + " " + lastName + "! Welcome. This is your lucky number:";
+document.write(message);
+
+document.write(6 + 2); //this is an expression
+    
